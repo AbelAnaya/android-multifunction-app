031637ca48827af6c58895a96a0e189915090e5a
abelanaya <abelanaya97@gmail.com>
2021-06-15 00:10:23 +0200

    ListView de cryptocurrency implementada en HomeFragment. Pendiente refactor para el nombre del fragment y layouts

--- app/src/main/java/com/upm/pasproject/MiSimpleAdapter.java
+++ app/src/main/java/com/upm/pasproject/MiSimpleAdapter.java
@@ -1,8 +1,13 @@
 package com.upm.pasproject;
 
 import android.content.Context;
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
@@ -11,21 +16,43 @@ import java.util.List;
 import java.util.Map;
 
 public class MiSimpleAdapter extends SimpleAdapter {
+    private List<HashMap<String, String>> datos;
+    private Context context;
 
     static class ViewHolder{
-        
+        TextView crypto;
+        TextView value;
+        ImageView icon_url;
     }
 
     public MiSimpleAdapter(Context context, List<? extends Map<String, ?>> data, int resource, String[] from, int[] to) {
         super(context, data, resource, from, to);
+        datos = (List<HashMap<String, String>>) data;
+        this.context = context;
     }
 
     @Override
-    public void setViewImage(ImageView v, String value) {
-        super.setViewImage(v, value);
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder vh;
+        if(convertView == null){
+            convertView = LayoutInflater.from(context).inflate(R.layout.row, null);
+            vh = new ViewHolder();
+            vh.crypto = convertView.findViewById(R.id.crypto);
+            vh.value = convertView.findViewById(R.id.value);
+            vh.icon_url = convertView.findViewById(R.id.icon_url);
+            convertView.setTag(vh);
+        } else {
+            vh = (ViewHolder) convertView.getTag();
+        }
+
+        HashMap<String, String> dato = datos.get(position);
+        vh.crypto.setText(dato.get("crypto"));
+        vh.value.setText(dato.get("value"));
         /*Picasso.get()
-                .load(value)
+                .load(dato.get("icon_url"))
                 .resize(150,150)
-                .into(v);*/
+                .into(vh.icon_url);*/
+
+        return convertView;
     }
 }
--- app/src/main/java/com/upm/pasproject/ui/home/HomeFragment.java
+++ app/src/main/java/com/upm/pasproject/ui/home/HomeFragment.java
@@ -4,42 +4,54 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.upm.pasproject.MiSimpleAdapter;
 import com.upm.pasproject.R;
 import com.upm.pasproject.databinding.FragmentHomeBinding;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
-    private FragmentHomeBinding binding;
+    private String APIURL =
+            "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        ConstraintLayout constraintLayout = (ConstraintLayout) inflater.inflate(R.layout.fragment_home,null);
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
-    }
+        ListView listView = (ListView) constraintLayout.findViewById(R.id.listview);
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+        List<HashMap<String,String>> datos = new ArrayList<>();
+
+        HashMap<String,String> hm = new HashMap<>();
+        for (int i = 0; i<15;i++){
+            hm.put("crypto","Ethereum (ETH)");
+            hm.put("value",""+(8023.65+1000*i)+"$");
+            hm.put("icon_url", "https://cryptoicons.org/api/black/eth/200");
+            datos.add(hm);
+        }
+
+        SimpleAdapter adapter = new MiSimpleAdapter(container.getContext(),
+                datos, //datos
+                R.layout.row, //layout
+                new String[] {"crypto","value", "icon_url"}, //from
+                new int[] {R.id.crypto, R.id.value, R.id.icon_url} //to
+        );
+
+        listView.setAdapter(adapter);
+        return constraintLayout;
     }
 }
